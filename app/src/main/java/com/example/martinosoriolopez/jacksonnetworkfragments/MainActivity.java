039e83eff83c139e3e765e4fb9e5d1e9a4123e83
martinosoriolopez <me@martinosorio.com>
2016-12-23 09:53:32 -0500

    Network

---
+++ app/src/main/java/com/example/martinosoriolopez/jacksonnetworkfragments/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.martinosoriolopez.jacksonnetworkfragments;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckNetworkClick(View view) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if((networkInfo != null) && networkInfo.isConnectedOrConnecting()){
+            Toast.makeText(this, "YOU HAVE NETWORK!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onJacksonClick(View view) {
+    }
+
+    public void onFragmentsClick(View view) {
+    }
+}
